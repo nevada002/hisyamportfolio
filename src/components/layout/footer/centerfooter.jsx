a596f63cbb0a51aee96d389038f26715e6d65e19
Juliant Daniel Latuary <daniel.latuary@gmail.com>
2022-09-25 05:55:59 +0700

    add footer

--- src/components/layout/footer/centerfooter.jsx
+++ src/components/layout/footer/centerfooter.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMedium,
+  faInstagram,
+  faLinkedin,
+  faDribbble,
+} from "@fortawesome/free-brands-svg-icons";
 
 export default function CenterFooter() {
   return (
@@ -15,7 +22,43 @@ export default function CenterFooter() {
         >
           <h1>Let’s Connect</h1>
           <p>For Work inquiry or just say hello</p>
-          <p>hawari.hisyam@gmail.com</p>
+          <a
+            href="mailto: hawari.hisyam@gmail.com"
+            target="_blank"
+            rel="noreferrer"
+            style={{ textDecoration: "none" }}
+          >
+            <p>hawari.hisyam@gmail.com</p>
+          </a>
+        </Col>
+      </Row>
+      <Row>
+        <Col
+          xs={12}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            textAlign: "center",
+            marginTop: "50px",
+            marginBottom: "100px",
+          }}
+        >
+          <a href="https://www.instagram.com/" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon
+              className="mx-3"
+              color="black"
+              icon={faInstagram}
+            />
+          </a>
+          <a href="https://www.linkedin.com/" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon className="mx-3" color="black" icon={faLinkedin} />
+          </a>
+          <a href="https://dribbble.com/" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon className="mx-3" color="black" icon={faDribbble} />
+          </a>
+          <a href="https://medium.com/" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon className="mx-3" color="black" icon={faMedium} />
+          </a>
         </Col>
       </Row>
     </>
